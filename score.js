e03d9e0cabe04cac1b74804e05aa22c7b8f9032b
Ge Cirou <ca_gerardo@hotmail.com>
2024-01-29 06:35:16 -0300

    base

--- score.js
+++ score.js
@@ -11,24 +11,28 @@ document.getElementById("label_ancar").innerHTML = ancares;
 function bancaGana() {
     bancas += 1;
     pases += 1;
+    let count = "B" + pases;
     document.getElementById("label_banca").innerHTML = bancas;
     document.getElementById("label_pase").innerHTML = pases;
-    document.getElementById("B1").style.backgroundColor = "#000";
+    document.getElementById(count).style.backgroundColor = "#000";
 }
 function puntoGana() {
     puntos += 1;
     pases += 1;
+    let count = "P" + pases;
     document.getElementById("label_punto").innerHTML = puntos;
     document.getElementById("label_pase").innerHTML = pases;
-    document.getElementById("P1").style.backgroundColor = "#000";
+    document.getElementById(count).style.backgroundColor = "#000";
 }
 function ancarDio() {
     ancares += 1;
     pases += 1;
+    let countB = "B" + pases;
+    let countP = "P" + pases;
     document.getElementById("label_ancar").innerHTML = ancares;
     document.getElementById("label_pase").innerHTML = pases;
-    document.getElementById("B1").style.backgroundColor = "#000";
-    document.getElementById("P1").style.backgroundColor = "#000";
+    document.getElementById(countB).style.backgroundColor = "#000";
+    document.getElementById(countP).style.backgroundColor = "#000";
 }
 
 function reset() {
@@ -44,4 +48,14 @@ function reset() {
 
     document.getElementById("B1").style.backgroundColor = "#fff";
     document.getElementById("P1").style.backgroundColor = "#fff";
+    document.getElementById("B2").style.backgroundColor = "#fff";
+    document.getElementById("P2").style.backgroundColor = "#fff";
+    document.getElementById("B3").style.backgroundColor = "#fff";
+    document.getElementById("P3").style.backgroundColor = "#fff";
+    document.getElementById("B4").style.backgroundColor = "#fff";
+    document.getElementById("P4").style.backgroundColor = "#fff";
+    document.getElementById("B5").style.backgroundColor = "#fff";
+    document.getElementById("P5").style.backgroundColor = "#fff";
+    document.getElementById("B6").style.backgroundColor = "#fff";
+    document.getElementById("P6").style.backgroundColor = "#fff";
 }
