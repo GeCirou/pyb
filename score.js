dcdb422e9a99173914f7c636b8c77f993426c28e
Ge Cirou <ca_gerardo@hotmail.com>
2024-01-29 16:07:55 -0300

    boton back

--- score.js
+++ score.js
@@ -3,6 +3,8 @@ var bancas = 0;
 var puntos = 0;
 var ancares = 0;
 
+var historial =[];
+
 document.getElementById("label_pase").innerHTML = pases;
 document.getElementById("label_banca").innerHTML = bancas;
 document.getElementById("label_punto").innerHTML = puntos;
@@ -11,7 +13,11 @@ document.getElementById("label_ancar").innerHTML = ancares;
 function bancaGana() {
     bancas += 1;
     pases += 1;
+
     let count = "B" + pases;
+
+    historial.push(1);
+
     document.getElementById("label_banca").innerHTML = bancas;
     document.getElementById("label_pase").innerHTML = pases;
     document.getElementById(count).style.backgroundColor = "#000";
@@ -19,7 +25,11 @@ function bancaGana() {
 function puntoGana() {
     puntos += 1;
     pases += 1;
+
     let count = "P" + pases;
+
+    historial.push(2);
+
     document.getElementById("label_punto").innerHTML = puntos;
     document.getElementById("label_pase").innerHTML = pases;
     document.getElementById(count).style.backgroundColor = "#000";
@@ -27,12 +37,60 @@ function puntoGana() {
 function ancarDio() {
     ancares += 1;
     pases += 1;
+
     let countB = "B" + pases;
     let countP = "P" + pases;
+
+    historial.push(3);
+
     document.getElementById("label_ancar").innerHTML = ancares;
     document.getElementById("label_pase").innerHTML = pases;
     document.getElementById(countB).style.backgroundColor = "#000";
     document.getElementById(countP).style.backgroundColor = "#000";
+}
+
+function volver() {
+    if (historial[historial.length-1] == 1){
+        let count = "B" + pases;
+
+        bancas -= 1;
+        pases -= 1;
+
+        historial.splice(historial.length-1, 1);
+
+        document.getElementById("label_banca").innerHTML = bancas;
+        document.getElementById("label_pase").innerHTML = pases;
+        document.getElementById(count).style.backgroundColor = "#fff";
+    }
+    else if (historial[historial.length-1] == 2){
+        let count = "P" + pases;
+
+        puntos -= 1;
+        pases -= 1;
+
+        historial.splice(historial.length-1, 1);
+
+        document.getElementById("label_punto").innerHTML = puntos;
+        document.getElementById("label_pase").innerHTML = pases;
+        document.getElementById(count).style.backgroundColor = "#fff";
+    }
+    else if (historial[historial.length-1] == 3){
+        let countB = "B" + pases;
+        let countP = "P" + pases;
+
+        ancares -= 1;
+        pases -= 1;
+
+        historial.splice(historial.length-1, 1);
+
+        document.getElementById("label_ancar").innerHTML = ancares;
+        document.getElementById("label_pase").innerHTML = pases;
+        document.getElementById(countB).style.backgroundColor = "#fff";
+        document.getElementById(countP).style.backgroundColor = "#fff";
+    }
+    else {
+        alert("aún no pasó nada")
+    }
 }
 
 function reset() {
